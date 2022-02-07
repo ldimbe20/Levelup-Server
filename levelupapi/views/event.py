707da77b707e04805028bd81ce2595f440a6de54
ldimbe20 <ldimbe20@gmail.com>
2022-02-07 16:10:52 -0600

    Rename

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
+from levelupapi.models import Game
+from levelupapi.models import Gamer
 
 
 class EventView(ViewSet):
@@ -35,6 +37,51 @@ class EventView(ViewSet):
             events = events.filter(game_id=game)
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        organizer = Gamer.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            game=game,
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            organizer=organizer,
+        )
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        event= Event.objects.get(pk=pk)
+        event.description=request.data["description"],
+        event.date=request.data["date"],
+        event.time=request.data["time"],
+        
+        game=Game.objects.get(pk=request.data["game"])
+        event.game= game
+        event.save()
+        
+        organizer=Gamer.objects.get(pk=request.data["gamer"])
+        event.organizer= organizer
+        event.save()
+        
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    
+    
+    
 
 
 class EventSerializer(serializers.ModelSerializer):
--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -3,7 +3,9 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Game
-
+from levelupapi.models import GameType
+from levelupapi.models import Gamer
+from django.core.exceptions import ValidationError
 
 class GameView(ViewSet):
     """Level up game types view"""
@@ -13,17 +15,82 @@ class GameView(ViewSet):
         serializer = GameSerializer(game)
         return Response(serializer.data)
     
+    # ABOVE IS FUNCTION TO GET ITEM BY ID
+    
 
     def list(self, request):
         games = Game.objects.all()
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
+    
+    # def create(self, request):
+    #     """Handle POST operations
 
+    #     Returns:
+    #         Response -- JSON serialized game instance
+    #     """
+    #     gamer = Gamer.objects.get(user=request.auth.user)
+    #     game_type = GameType.objects.get(pk=request.data["game_type"])
+
+    #     game = Game.objects.create(
+    #         title=request.data["title"],
+    #         maker=request.data["maker"],
+    #         number_of_players=request.data["number_of_players"],
+    #         skill_level=request.data["skill_level"],
+    #         gamer=gamer,
+    #         game_type=game_type
+    #     )
+    #     serializer = GameSerializer(game)
+    #     return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            number_of_players=request.data["number_of_players"],
+            skill_level=request.data["skill_level"],
+            gamer=gamer,
+            game_type=game_type
+        )
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+    
+    # def update(self, request, pk):
+    #     """Handle PUT requests for a game
+
+    #     Returns:
+    #         Response -- Empty body with 204 status code
+    #     """
+
+    #     game = Game.objects.get(pk=pk)
+    #     game.title = request.data["title"]
+    #     game.maker = request.data["maker"]
+    #     game.number_of_players = request.data["number_of_players"]
+    #     game.skill_level = request.data["skill_level"]
+
+    #     game_type = GameType.objects.get(pk=request.data["gameTypeId"])
+    #     game.game_type = game_type
+    #     game.save()
+
+    #     return Response(None, status=status.HTTP_204_NO_CONTENT)
+   
+
+
+        
 
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
     class Meta:
         model = Game
-        fields = ('id', 'title', 'maker', 'number_of_players', 'skill_level', 'game_type_id', 'gamer_id')
-      
+        fields = '__all__'
+        # fields = ('id', 'title', 'maker', 'number_of_players', 'skill_level', 'gamer_id','game_type_id')
+        # depth = 2
