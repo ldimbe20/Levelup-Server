e4f9036ace164298a4d3d324738ba2c901f5b704
ldimbe20 <ldimbe20@gmail.com>
2022-02-10 11:58:58 -0600

    Created action of event

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     description = models.TextField()
@@ -7,3 +8,11 @@ class Event(models.Model):
     time = models.TimeField()
     organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='organizing')
     attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
