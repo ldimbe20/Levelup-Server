2e79c6b342f283532aabdce6f2ee29cef0a85a60
ldimbe20 <ldimbe20@gmail.com>
2022-02-23 13:44:48 -0600

    Added python html for events

--- levelupreports/urls.py
+++ levelupreports/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import UserGameList
+from .views import UserEventList
 
 urlpatterns = [
     path('reports/usergames', UserGameList.as_view()),
+    path('reports/userevents', UserEventList.as_view()),
 ]
+
+
